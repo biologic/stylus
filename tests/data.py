6039a709a31a55e4b93f2fefea703f04af3e2460
winstonewert <winstonewert@2b6d04c7-de4b-0410-a79d-f76cffd13e6f>
2011-06-06 18:01:51 +0000

    Added doug's test

--- tests/data.py
+++ tests/data.py
@@ -1,7 +1,8 @@
 # The following is a list of gene-plan combinations which should
 # not be run
 BLACKLIST = [
-    ('8C58', 'performance') # performance.xml make specific references to 52DC
+    ('8C58', 'performance'), # performance.xml make specific references to 52DC
+    ('7DDA', 'performance') # performance.xml make specific references to 52DC
 ]
 
 IGNORE = {
