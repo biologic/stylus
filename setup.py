6645d7a3e677a25619c672fdeb8876ba06a5c8bc
Winston Ewert <winstonewert@gmail.com>
2013-08-02 14:32:25 -0700

    Linking against uuid

--- setup.py
+++ setup.py
@@ -4,6 +4,14 @@ import glob
 
 sources = glob.glob('src/*.cpp') + glob.glob('src/randomc/*.cpp') + glob.glob('src/*.i')
 sources = [source for source in sources if '_wrap' not in source]
+
+if platform.system() == 'Darwin':
+    libraries = []
+elif platform.system() == 'Linux':
+    libraries = ['uuid']
+else:
+    libraries = [] # hope for the best
+
 setup(
     name = 'stylusengine',
     ext_modules = [
@@ -13,7 +21,8 @@ setup(
             extra_compile_args = subprocess.check_output(['xml2-config', '--cflags']).split(),
             extra_link_args = subprocess.check_output(['xml2-config', '--libs']).split(),
             swig_opts = ['-c++'],
-            include_dirs = ['src']
+            include_dirs = ['src'],
+            libraries = libraries
         )
     ],
     py_modules = ['stylusengine'],
